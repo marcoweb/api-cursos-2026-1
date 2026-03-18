7158622ff3e30f393fdcc58b32c30c4783bde1a7
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-03-18 18:51:31 +0000

    Aula 18/03/2026

--- src/main/java/app/autenticacao/TokenService.java
+++ src/main/java/app/autenticacao/TokenService.java
@@ -1,5 +1,7 @@
 package app.autenticacao;
 
+import java.security.AlgorithmConstraints;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -12,5 +14,37 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class TokenService {
+    private String tokenKey = "123456789";
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(
+            ZoneOffset.of("-03:00"));
+    }
+
+    public String generateToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(tokenKey);
+            return JWT.create()
+                .withIssuer("FATEC API")
+                .withSubject(usuario.getNomeDeUsuario())
+                .withExpiresAt(this.expirationDate())
+                .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar JWT");
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(tokenKey);
+            return JWT.require(algorithm)
+            .withIssuer("FATEC API")
+            .verify(token)
+            .getSubject();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Token Inválido");
+        }
+
+    }
 
 }
