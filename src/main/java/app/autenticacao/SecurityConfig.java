9c2c649be8809afd06a383b8b24934a9b263b356
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2026-03-24 19:01:53 +0000

    Aula 24/03/2026

--- src/main/java/app/autenticacao/SecurityConfig.java
+++ src/main/java/app/autenticacao/SecurityConfig.java
@@ -5,7 +5,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig 
+public class SecurityConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "auth").permitAll()
+                .anyRequest().authenticated())
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }    
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration) throws Exception {
+            return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder PasswordEncoder() {
